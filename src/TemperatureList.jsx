0ea56e3e99907d92662a732a9c4d221794e91f30
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-09-20 16:53:21 +0200

    emojis added

--- src/TemperatureList.jsx
+++ src/TemperatureList.jsx
@@ -1,14 +1,28 @@
-function TemperatureList({ temperatures }) {
-  return (
-    <div className="card-container">
-      {temperatures.map((temp, index) => (
-        <div className="card" key={index}>
-          <h5>{`Hour ${index}:00`}</h5>
-          <p>{`Temperature: ${temp}°C`}</p>
-        </div>
-      ))}
-    </div>
-  );
-}
-
-export default TemperatureList;
+const getWeatherEmoji = (temp) => {
+    if (temp <= 10) {
+      return "❄️"; 
+    } else if (temp >= 10 && temp <= 15) {
+      return "⚡";
+    } else if (temp >= 16 && temp <= 20) {
+      return "🌧️"; 
+    } else {
+      return "☀️";
+    }
+  }
+  
+  const TemperatureList = ({ temperatures }) => {
+    return (
+      <div className="card-container">
+        {temperatures.map((temp, index) => (
+          <div className="card" key={index}>
+            <h5>{`Hour ${index}:00`}</h5>
+            <p>{`Temperature: ${temp}°C`}</p>
+            <p>{`Condition: ${getWeatherEmoji(temp)}`}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
+  export default TemperatureList;
+  
